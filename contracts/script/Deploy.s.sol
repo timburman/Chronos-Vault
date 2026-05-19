6254d34ae2b4c8b7eb8ea6e34f8e12427ed9c8d8
aryan <aryankaushik251@gmail.com>
2026-05-19 10:56:13 +0530

    chore: update deploy script for base sepolia

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -6,11 +6,7 @@ import {VaultFactory} from "../src/VaultFactory.sol";
 
 contract DeployFactory is Script {
     function run() external {
-        // Anvil's first default private key (account[0])
-        // 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-        uint256 deployerKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-
-        vm.startBroadcast(deployerKey);
+        vm.startBroadcast();
 
         VaultFactory factory = new VaultFactory();
         console.log("VaultFactory deployed at:", address(factory));
